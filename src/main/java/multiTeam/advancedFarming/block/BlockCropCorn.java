47fae4b18610993b9446cd7f05e7858c0e6f145b
Robert Sandfield <robert.f.sandfield@gmail.com>
2017-12-20 07:36:00 -0500

    Corn mostly works. Just a little bit asplody.

--- src/main/java/multiTeam/advancedFarming/block/BlockCropCorn.java
+++ src/main/java/multiTeam/advancedFarming/block/BlockCropCorn.java
@@ -6,11 +6,13 @@ import multiteam.advancedfarming.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.BlockDoublePlant;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.IStringSerializable;
@@ -51,20 +53,32 @@ public class BlockCropCorn extends BlockCrops {
 
                     if(net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt((int)(25.0F / f) + 1) == 0))
                     {
-                    	if (i < 2) {
-                            worldIn.setBlockState(pos, this.withAge(i + 1), 2);
-                            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state, worldIn.getBlockState(pos));	
-                    	} else {
-                    		if(worldIn.isAirBlock(pos.up()) || worldIn.getBlockState(pos.up()).getBlock() == this) {
-                    			worldIn.setBlockState(pos, this.withAge(i + 1), 2);
-                    			worldIn.setBlockState(pos.up(), this.withAge(i + 1), 2);
-                                net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state, worldIn.getBlockState(pos));
-                    		}
-                    	}
+                    	BlockCropCorn cropcorn = (BlockCropCorn) worldIn.getBlockState(pos).getBlock();
+                    	cropcorn.grow(worldIn, pos, state);
+                        net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state, worldIn.getBlockState(pos));
                     }
                 }
             }
     	}
+    }
+    
+    public void grow(World worldIn, BlockPos pos, IBlockState state) {
+    	if (state.getValue(HALF) == BlockCropCorn.EnumBlockHalf.LOWER) {
+    		super.grow(worldIn, pos, state);
+    		if(this.getAge(state) >= 2) {
+    			 worldIn.setBlockState (pos.up(), worldIn.getBlockState(pos).withProperty(getHalfProperty(), BlockCropCorn.EnumBlockHalf.UPPER));
+    		}
+    	} else {
+    		IBlockState iblockstate = worldIn.getBlockState(pos.down());
+    		if (iblockstate.getBlock() instanceof BlockCropCorn) {
+    			BlockCropCorn lower = (BlockCropCorn)iblockstate.getBlock();
+    			lower.grow(worldIn, pos.down(), iblockstate);
+            }
+    	}
+    }
+    
+    public PropertyEnum getHalfProperty () {
+    	return HALF;
     }
     
     /*
@@ -73,11 +87,12 @@ public class BlockCropCorn extends BlockCrops {
      */
     protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
     	if (!this.canBlockStay(worldIn, pos, state)) {
+    		Minecraft.getMinecraft().player.sendChatMessage("OH NOES");
             boolean flag = state.getValue(HALF) == BlockCropCorn.EnumBlockHalf.UPPER;
-            BlockPos blockpos = flag ? pos : pos.up();
-            BlockPos blockpos1 = flag ? pos.down() : pos;
-            Block block = (Block)(flag ? this : worldIn.getBlockState(blockpos).getBlock());
-            Block block1 = (Block)(flag ? worldIn.getBlockState(blockpos1).getBlock() : this);
+            BlockPos blockpos = flag ? pos : pos.up();		//If flagged for upper, use pos. If not, use the next pos up.
+            BlockPos blockpos1 = flag ? pos.down() : pos;	//If flagged for upper, uses the next pos down. If not, uses pos.
+            Block block = (Block)(flag ? this : worldIn.getBlockState(blockpos).getBlock());	//Makes sure to always be the upper block
+            Block block1 = (Block)(flag ? worldIn.getBlockState(blockpos1).getBlock() : this);	//Makes sure to always be the lower block
 
             if (!flag) this.dropBlockAsItem(worldIn, pos, state, 0); //Forge move above the setting to air.
 
@@ -91,18 +106,13 @@ public class BlockCropCorn extends BlockCrops {
         }
     }
     
-    /*
-     * Stolen from BlockDoublePlant without modification beyond the obvious
-     * @see net.minecraft.block.BlockCrops#canBlockStay(net.minecraft.world.World, net.minecraft.util.math.BlockPos, net.minecraft.block.state.IBlockState)
-     */
     public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state)
     {
         if (state.getBlock() != this) return super.canBlockStay(worldIn, pos, state); //Forge: This function is called during world gen and placement, before this block is set, so if we are not 'here' then assume it's the pre-check.
         if (state.getValue(HALF) == BlockCropCorn.EnumBlockHalf.UPPER) {
-            return worldIn.getBlockState(pos.down()).getBlock() == this;
+            return worldIn.getBlockState(pos.down()).getBlock() == ModBlocks.CROP_CORN;
         } else {
-            IBlockState iblockstate = worldIn.getBlockState(pos.up());
-            return iblockstate.getBlock() == this && super.canBlockStay(worldIn, pos, iblockstate);
+            return super.canBlockStay(worldIn, pos, state);
         }
     }
     
